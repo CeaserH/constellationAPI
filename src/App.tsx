fb63e745b640b09f21376fa27ad9754426eb04f9
Ceaser Hernandez <45456228+CeaserH@users.noreply.github.com>
2025-11-19 21:25:54 -0800

    Initial commit, creation of constellation history tracker

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { fetchConstellationHistory } from './api/constellation';
 
 function App() {
+  const [history, setHistory] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      const result = await fetchConstellationHistory();
+      setHistory(result);
+      setLoading(false);
+    })();
+  }, []);
+
+  if(loading) return <div>Loading Constellation Data...</div>
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: "20px"}}>
+      <h1>Cosntellation Flight History</h1>
+      {history.map((entry) => (
+        <div key={entry.hourAgo} style={{ marginBottom: "20px"}}>
+          <h3>{entry.hourAgo} hours ago.</h3>
+          <pre>{JSON.stringify(entry.data, null, 2)}</pre>
+        </div>
+      ))}
     </div>
   );
 }
---
+++ src/api/constellation.ts
@@ -0,0 +1,24 @@
+export async function fetchConstellationHistory(){
+
+    const history = [];
+
+    for(let i = 0; i <= 23; i++){
+        const hour = i.toString().padStart(2, '0');
+        const url = 'https://a.windbornesystems.com/treasures/${hour}.json';
+
+        try{
+            const response = await fetch(url);
+            if(!response.ok) continue;
+
+            const data = await response.json();
+            history.push({
+                hourAgo: i,
+                data
+            });
+        } catch (err){
+            console.error('Error fetching ${hour}.json:', err);
+        }
+    }
+
+    return history;
+}
