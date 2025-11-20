a83b926467a6c88c0de91a3e63fbc5136c2ac612
Ceaser Hernandez <45456228+CeaserH@users.noreply.github.com>
2025-11-19 21:40:58 -0800

    Changes from queries being inside of a for loop, to a singular query at 01 hours ago.

--- src/api/constellation.ts
+++ src/api/constellation.ts
@@ -2,23 +2,19 @@ export async function fetchConstellationHistory(){
 
     const history = [];
 
-    for(let i = 0; i <= 23; i++){
-        const hour = i.toString().padStart(2, '0');
-        const url = `https://a.windbornesystems.com/treasure/${hour}.json`;
-
         try{
-            const response = await fetch(url);
-            if(!response.ok) continue;
-
+            const response = await fetch("https://a.windbornesystems.com/treasure/01.json");
             const data = await response.json();
-            history.push({
-                hourAgo: i,
-                data
-            });
+
+            return[
+                {
+                    hourAgo: 0,
+                    data
+                }
+            ];
+
         } catch (err){
             console.error(`Error fetching ${hour}.json:`, err);
+            return [];
         }
-    }
-
-    return history;
 }
